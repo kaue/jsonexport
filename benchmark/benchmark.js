0a269c884968801875e208fe2dc73dc0562ee73b
Kauê Gimenes <kauegimenes@users.noreply.github.com>
2017-06-08 20:07:46 -0300

    jslint fix

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,9 +1,7 @@
 var fs = require('fs');
 var util = require('util');
 var Benchmark = require('benchmark');
-var suite = new Benchmark.Suite;
-var json2csv = require('json2csv');
-var json2csvstream = new require('json2csv-stream')();
+var suite = new Benchmark.Suite();
 var jsonexport = require('../lib/index');
 // start benchmarking
 suite
@@ -20,9 +18,9 @@ suite
           fs.writeFile('out-no-streams.csv', csv, function(err) {
             if (err) console.log(err);
             deferred.resolve();
-          })
-        })
-      })
+          });
+        });
+      });
     }
   })
   .add('jsonexport-stream', {
@@ -44,52 +42,6 @@ suite
       });
 
       reader.pipe(jsonexport()).pipe(writer);
-    }
-  })
-  .add('json2csv', {
-    'defer': true,
-    'fn': function(deferred) {
-      // var data = require('./data.json');
-      fs.readFile('data.json', function(err, data) {
-        data = JSON.parse(data.toString());
-        if (err) console.log(err);
-        json2csv({
-          data: data,
-          fields: ['car', 'price', 'color']
-        }, function(err, csv) {
-          if (err) console.log(err);
-          fs.writeFile('out-no-streams.csv', csv, function(err) {
-            if (err) console.log(err);
-            deferred.resolve();
-          })
-        })
-      })
-    }
-  })
-  .add('json2csv-stream', {
-    'defer': true,
-    'fn': function(deferred) {
-      var reader = fs.createReadStream('data.json');
-      var writer = fs.createWriteStream('out.csv');
-
-      reader.on('error', function(err) {
-        console.log(err);
-      });
-
-      writer.on('error', function(err) {
-        console.log(err);
-      });
-      json2csvstream.on('end', function() {
-        // Wait until reader is over and then close reader and finish deferred test
-        writer.end();
-        deferred.resolve();
-      });
-  
-
-
-      reader.pipe(json2csvstream).pipe(writer, {
-        end: false
-      });
     }
   })
   // add listeners
