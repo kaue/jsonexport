d2490bc38843e5a8687ca8ee12cf61f5787fc672
Acker Apple <ackerapple@Ackers-Mac-mini.local>
2020-05-25 11:35:05 -0400

    update jsonexport. smart url demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,12 +1,12 @@
+const version = require("jsonexport/package.json").version;
 const jsonexport = require("jsonexport/dist")
 const errElm = document.getElementById('error-elm')
 const jSrcElm = document.getElementById('json-textarea')
 const outElm = document.getElementById('output-textarea')
 
 function update(){
-
   try{
-    var ob = JSON.parse(jSrcElm.value.replace(/((^\s)|(\s$))/g,''))
+    var ob = JSON.parse(jSrcElm.value.replace(/((^\s)|(\s$))/g,''));
   }catch(e){
     return handleError(e)
   }
@@ -16,7 +16,7 @@ function update(){
 
 function handleExport(err, data){
   if(err){
-    return handlerError(err)
+    return handleError(err)
   }else{
     errElm.style.display='none'
   }
@@ -27,7 +27,10 @@ function handleExport(err, data){
     saveAs(blob, fileName)
   */
 
-  outElm.value = data
+  outElm.value = data; // update output display
+  
+  // sharable url
+  window.history.replaceState(null, null, window.location.pathname + "?json=" + jSrcElm.value); 
 }
 
 function handleError(err){
@@ -36,6 +39,38 @@ function handleError(err){
   errElm.style.display=''
 }
 
+function populateVersion() {
+  const versions = [];
+  Array.prototype.push.apply(versions, document.getElementsByClassName('versionOutput'))
+  versions.forEach(function(e) {
+    e.innerHTML = version;
+  });
+}
+
+function getUrlParams(url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+
+function loadUrlParams() {
+  const params = getUrlParams(window.location.href);
+
+  if (params.json) {
+    jSrcElm.value = params.json
+  }
+
+  update()
+}
+
 jSrcElm.addEventListener('keyup', update)
 
-update()
+populateVersion();
+loadUrlParams();
