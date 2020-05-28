7229b038eb698b38d69f279330f8b1dc454fc47e
Acker Apple <ackerapple@Ackers-Mac-mini.local>
2020-05-27 23:21:38 -0400

    options in html

--- demo/src/index.js
+++ demo/src/index.js
@@ -29,36 +29,52 @@ const defaultData = [{
 "nickname": "dmartin"
 }];
 
+// allow html access to methods
+window.update = update;
+window.downloadResult = downloadResult;
 
 function update(){
+  var json = "";
+
   try{
-    var ob = JSON.parse(jSrcElm.value.replace(/((^\s)|(\s$))/g,''));
+    json = JSON.parse(jSrcElm.value.replace(/((^\s)|(\s$))/g,''));
   }catch(e){
     return handleError(e)
   }
 
-  jsonexport(ob, function(err,data) {
-    handleExport(err, data, ob);
+  const options = getOptions();
+
+  jsonexport(json, options, function(err,data) {
+    if (err) {
+      return handleError(err)
+    }else{
+      errElm.style.display = 'none'
+    }
+
+    outElm.value = data; // update output display
+
+    updateDownload();
+    updateUrl(json, options);
   });
 }
 
-function handleExport(err, data, original){
-  if(err){
-    return handleError(err)
-  }else{
-    errElm.style.display='none'
-  }
-
-  //example of to blob
-  /*
-    var blob = new Blob([data], {type: "text/comma-separated-values;charset=utf-8"});
-    saveAs(blob, fileName)
-  */
-
-  outElm.value = data; // update output display
+function updateUrl(json, options) {
+  const jsonString = JSON.stringify(json);
+  const jsonOptions = JSON.stringify(options);
   
-  // sharable url
-  window.history.replaceState(null, null, window.location.pathname + "?json=" + JSON.stringify(original, null, 2)); 
+  const urlVars = "json=" + jsonString + "&options=" + jsonOptions;
+  const newUrl = window.location.pathname + "?" + urlVars;
+  
+  window.history.replaceState(null, null, newUrl);
+}
+
+function updateDownload() {
+  const data = outElm.value;
+  var blob = new Blob([data], {type: "text/comma-separated-values;charset=utf-8"});
+  const fileName = "jsonexport" + Date.now() + ".csv";
+  const target = document.getElementById("downloadResult");
+  target.href =  URL.createObjectURL(blob);
+  target.download = fileName;
 }
 
 function handleError(err){
@@ -103,7 +119,71 @@ function loadUrlParams() {
   update()
 }
 
-jSrcElm.addEventListener('keyup', update)
+function getOptions() {
+  const options = {};
+  const inputArray = [];
+  Array.prototype.push.apply(inputArray, document.getElementById('options-wrap').getElementsByTagName("input"));
 
-populateVersion();
-loadUrlParams();
+  inputArray.forEach(function(input){
+    if (input.type === 'checkbox') {
+      if (input.checked) {
+        options[input.getAttribute('name')] = input.value === "false" ? false : true;
+      }
+    } else {
+      if (input.value) {
+        options[input.getAttribute('name')] = input.value;
+      }
+    }
+  });
+
+  return options;
+}
+
+function populateOptionWrap() {
+  const wrap = document.getElementById('options-wrap');
+  const template = document.getElementById('options-template');
+  wrap.innerHTML = template.innerHTML;
+
+  const params = getUrlParams(window.location.href);
+
+  if (params.options) {    
+    var urlOptions = {};
+
+    try {
+      urlOptions = JSON.parse(params.options);
+    } catch (err) {
+      return console.error('Bad url options parameters', err);
+    }
+
+    const targets = [];
+    Array.prototype.push.apply(targets, wrap.getElementsByTagName('input'));
+
+    Object.keys(urlOptions).forEach(function(key){
+      const target = targets.find(function(input) {
+        return input.getAttribute('name') === key
+      });
+      
+      if (!target) {
+        return;
+      }
+
+      const value = urlOptions[key];
+      const type = target.getAttribute('type');
+      if (type==="checkbox") {
+        if (value.toString() == target.value) {
+          target.checked = true;
+        }
+      } else {
+        target.value = value;
+      }
+    });
+  }
+}
+
+function startApp() {
+  populateOptionWrap();
+  populateVersion();
+  loadUrlParams();
+}
+
+startApp();
